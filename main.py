4b7e8f1ac9865813b6bba9d517e708ff18287808
PedrooSam <pssa@cesar.school>
2024-09-22 15:37:13 -0300

    Adicionados comentários ao código

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from googleapiclient.discovery import build
 from os import system
 
-youtubeApiKey = "AIzaSyD3At1eJPVyPlYycyhV6BgCBfyBi8vhTho" #coloque aqui sua chave api
+youtubeApiKey = "" #COLOQUE AQUI SUA CHAVE API
 playlistId = "PLfoNZDHitwjUv0pjTwlV1vzaE0r7UDVDR"
 
 youtube = build("youtube", "v3", developerKey = youtubeApiKey)
 
+#pegando infomrações dos videos da playlist
 videos = youtube.playlistItems().list(part = "snippet", playlistId = playlistId, maxResults = 20).execute()
 playlist = videos["items"]
 
 videoViewsDict = {}
 
+#obtendo o nome e views de cada video e salvando no dicionário criado
 for video in playlist:
     videoId = video["snippet"]["resourceId"]["videoId"]
     videoName = video["snippet"]["title"]
@@ -18,6 +20,7 @@ for video in playlist:
     views = statisticResponse["items"][0]["statistics"].get("viewCount", 0)
     videoViewsDict[videoName] = views
 
+#ordenando o dicionário pelas views em ordem decrecente
 videoViewsDictSorted = dict(sorted(videoViewsDict.items(), key = lambda item: item[1], reverse = True))
 
 system("cls")
@@ -25,7 +28,7 @@ system("cls")
 print("Vídeos de \"Race Highlights 2024\" Ordenados por vizualizações:\n")
 
 i=1
-
+#imprimindo as informações salvas
 for key, value in videoViewsDictSorted.items():
     print(f"{i}º Vídeo\nTítulo: {key}\nVizualizações: {value}")
     print()
