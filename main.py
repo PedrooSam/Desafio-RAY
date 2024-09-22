129280867985e3471c06147b4b25f26cfd9246c5
PedrooSam <pssa@cesar.school>
2024-09-22 12:35:17 -0300

    Adicionados comentários explicando o código

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from googleapiclient.discovery import build
 from os import system
 
-youtubeApiKey = "AIzaSyD3At1eJPVyPlYycyhV6BgCBfyBi8vhTho"
-idPlaylist = "PLfoNZDHitwjUv0pjTwlV1vzaE0r7UDVDR"
+youtubeApiKey = "AIzaSyD3At1eJPVyPlYycyhV6BgCBfyBi8vhTho" #coloque aqui sua chave api
+playlistId = "PLfoNZDHitwjUv0pjTwlV1vzaE0r7UDVDR"
 
 youtube = build("youtube", "v3", developerKey = youtubeApiKey)
 
-videos = youtube.playlistItems().list(part = "snippet", playlistId = idPlaylist, maxResults = 20).execute()
+videos = youtube.playlistItems().list(part = "snippet", playlistId = playlistId, maxResults = 20).execute()
 playlist = videos["items"]
 
-dicionario = {}
+videoViewsDict = {}
 
 for video in playlist:
     videoId = video["snippet"]["resourceId"]["videoId"]
     videoName = video["snippet"]["title"]
     statisticResponse = youtube.videos().list(part = "statistics", id = videoId).execute()
     views = statisticResponse["items"][0]["statistics"].get("viewCount", 0)
-    dicionario[videoName] = views
+    videoViewsDict[videoName] = views
 
-dicionarioOrdenado = dict(sorted(dicionario.items(), key = lambda item: item[1], reverse = True))
+videoViewsDictSorted = dict(sorted(videoViewsDict.items(), key = lambda item: item[1], reverse = True))
 
 system("cls")
 
@@ -26,7 +26,7 @@ print("Vídeos de \"Race Highlights 2024\" Ordenados por vizualizações:\n")
 
 i=1
 
-for key, value in dicionarioOrdenado.items():
+for key, value in videoViewsDictSorted.items():
     print(f"{i}º Vídeo\nTítulo: {key}\nVizualizações: {value}")
     print()
     i = i+1
