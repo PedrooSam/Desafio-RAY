63099a71d1eb524cf90673b3d41328d5ab341d24
PedrooSam <pssa@cesar.school>
2024-09-22 12:04:59 -0300

    Breve alteração na lógica e ordenação dos vídeos

--- main.py
+++ main.py
@@ -9,16 +9,24 @@ youtube = build("youtube", "v3", developerKey = youtubeApiKey)
 videos = youtube.playlistItems().list(part = "snippet", playlistId = idPlaylist, maxResults = 20).execute()
 playlist = videos["items"]
 
-statisticVideos = []
+dicionario = {}
 
 for video in playlist:
-    idVideo = video["snippet"]["resourceId"]["videoId"]
-    statisticResponse = youtube.videos().list(part = "statistics", id = idVideo).execute()
-    statisticVideos.append(statisticResponse)
+    videoId = video["snippet"]["resourceId"]["videoId"]
+    videoName = video["snippet"]["title"]
+    statisticResponse = youtube.videos().list(part = "statistics", id = videoId).execute()
+    views = statisticResponse["items"][0]["statistics"].get("viewCount", 0)
+    dicionario[videoName] = views
+
+dicionarioOrdenado = dict(sorted(dicionario.items(), key = lambda item: item[1], reverse = True))
 
 system("cls")
 
-for video in statisticVideos:
-    views = video["items"][0]["statistics"].get("viewCount", 0)
-    print(views)
+print("Vídeos de \"Race Highlights 2024\" Ordenados por vizualizações:\n")
+
+i=1
+
+for key, value in dicionarioOrdenado.items():
+    print(f"{i}º Vídeo\nTítulo: {key}\nVizualizações: {value}")
     print()
+    i = i+1
