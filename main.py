1efadea010e66f7e5fa5948f3759431208b2daf9
PedrooSam <pssa@cesar.school>
2024-09-22 01:53:27 -0300

    Puxando as views dos videos

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from googleapiclient.discovery import build
 from os import system
 
-chaveApiYoutube = "AIzaSyD3At1eJPVyPlYycyhV6BgCBfyBi8vhTho"
+youtubeApiKey = "AIzaSyD3At1eJPVyPlYycyhV6BgCBfyBi8vhTho"
 idPlaylist = "PLfoNZDHitwjUv0pjTwlV1vzaE0r7UDVDR"
 
-youtube = build("youtube", "v3", developerKey = chaveApiYoutube)
+youtube = build("youtube", "v3", developerKey = youtubeApiKey)
 
 videos = youtube.playlistItems().list(part = "snippet", playlistId = idPlaylist, maxResults = 20).execute()
 playlist = videos["items"]
 
-system("cls")
+statisticVideos = []
 
 for video in playlist:
-    print(video["snippet"]["title"])
+    idVideo = video["snippet"]["resourceId"]["videoId"]
+    statisticResponse = youtube.videos().list(part = "statistics", id = idVideo).execute()
+    statisticVideos.append(statisticResponse)
+
+system("cls")
+
+for video in statisticVideos:
+    views = video["items"][0]["statistics"].get("viewCount", 0)
+    print(views)
+    print()
